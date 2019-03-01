646b518fb9815543f1d111714aa200d412858892
CASA\Pau <pau.florenti01@estudiant.upf.edu>
2019-03-01 15:46:24 +0100

    lab4 added

---
+++ Lab4/CG2018_p4/src/framework/application.cpp
@@ -0,0 +1,153 @@
+#include "application.h"
+#include "utils.h"
+#include "includes.h"
+#include "utils.h"
+
+#include "image.h"
+#include "mesh.h"
+#include "shader.h"
+#include "texture.h"
+#include "camera.h"
+#include "material.h"
+#include "light.h"
+
+Camera* camera = NULL;
+Mesh* mesh = NULL;
+Shader* shader = NULL;
+
+//might be useful...
+Material* material = NULL;
+Light* light = NULL;
+Shader* phong_shader = NULL;
+Shader* gouraud_shader = NULL;
+
+float angle = 0;
+
+Application::Application(const char* caption, int width, int height)
+{
+	this->window = createWindow(caption, width, height);
+
+	// initialize attributes
+	// Warning: DO NOT CREATE STUFF HERE, USE THE INIT 
+	// things create here cannot access opengl
+	int w,h;
+	SDL_GetWindowSize(window,&w,&h);
+
+	this->window_width = w;
+	this->window_height = h;
+	this->keystate = SDL_GetKeyboardState(NULL);
+}
+
+//Here we have already GL working, so we can create meshes and textures
+void Application::init(void)
+{
+	std::cout << "initiating app..." << std::endl;
+	
+	//here we create a global camera and set a position and projection properties
+	camera = new Camera();
+	camera->lookAt(Vector3(0,20,20),Vector3(0,10,0),Vector3(0,1,0));
+	camera->setPerspective(60,window_width / window_height,0.1,10000);
+
+	//then we load a mesh
+	mesh = new Mesh();
+	if( !mesh->loadOBJ( "lee.obj" ) )
+		std::cout << "FILE Lee.obj NOT FOUND " << std::endl;
+
+	//we load one or several shaders...
+	shader = new Shader();
+	shader->load( "simple.vs", "simple.ps" );
+
+	//load your Gouraud and Phong shaders here
+	//...
+
+	//CODE HERE:
+	//create a light (or several) and a materials
+	//...
+}
+
+//render one frame
+void Application::render(void)
+{
+	// Clear the window and the depth buffer
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable( GL_DEPTH_TEST );
+	glDepthFunc(GL_LEQUAL);
+
+	//Get the viewprojection matrix from our camera
+	Matrix44 viewprojection = camera->getViewProjectionMatrix();
+
+	//choose a shader and enable it
+	shader->enable();
+
+	Matrix44 model_matrix;
+	model_matrix.setIdentity();
+	model_matrix.translate(0,0,0); //example of translation
+	model_matrix.rotate(angle, Vector3(0, 1, 0));
+	shader->setMatrix44("model", model_matrix); //upload the transform matrix to the shader
+	shader->setMatrix44("viewprojection", viewprojection); //upload viewprojection info to the shader
+
+	//CODE HERE: pass all the info needed by the shader to do the computations
+	//send the material and light uniforms to the shader
+	//...
+
+	//do the draw call into the GPU
+	mesh->render(GL_TRIANGLES);
+
+	//disable shader when we do not need it any more
+	shader->disable();
+
+	//swap between front buffer and back buffer
+	SDL_GL_SwapWindow(this->window);
+}
+
+//called after render
+void Application::update(double seconds_elapsed)
+{
+	if (keystate[SDL_SCANCODE_SPACE])
+		angle += seconds_elapsed;
+
+	if (keystate[SDL_SCANCODE_RIGHT])
+		camera->eye = camera->eye + Vector3(1, 0, 0) * seconds_elapsed * 10.0;
+	else if (keystate[SDL_SCANCODE_LEFT])
+		camera->eye = camera->eye + Vector3(-1, 0, 0) * seconds_elapsed * 10.0;
+	if (keystate[SDL_SCANCODE_UP])
+		camera->eye = camera->eye + Vector3(0, 1, 0) * seconds_elapsed * 10.0;
+	else if (keystate[SDL_SCANCODE_DOWN])
+		camera->eye = camera->eye + Vector3(0, -1, 0) * seconds_elapsed * 10.0;
+}
+
+//keyboard press event 
+void Application::onKeyPressed( SDL_KeyboardEvent event )
+{
+	switch(event.keysym.sym)
+	{
+		case SDLK_ESCAPE: exit(0); break; //ESC key, kill the app
+		case SDLK_r: 
+			Shader::ReloadAll();
+			break; //ESC key, kill the app
+	}
+}
+
+//mouse button event
+void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse pressed
+	{
+	}
+
+}
+
+void Application::onMouseButtonUp( SDL_MouseButtonEvent event )
+{
+	if (event.button == SDL_BUTTON_LEFT) //left mouse unpressed
+	{
+
+	}
+}
+
+//when the app starts
+void Application::start()
+{
+	std::cout << "launching loop..." << std::endl;
+	launchLoop(this);
+}
